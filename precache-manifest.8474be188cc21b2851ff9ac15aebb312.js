134862de0958ba782db85d718ec32386e7bd683f
gauraviiitian <gauraviiitian@gmail.com>
2019-10-06 13:30:07 +0530

    Updates

---
+++ precache-manifest.8474be188cc21b2851ff9ac15aebb312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c242d312f21b1c4b0e732b3a05775a49",
+    "url": "/react-ant-project/index.html"
+  },
+  {
+    "revision": "744adbbca6078f34c2b1",
+    "url": "/react-ant-project/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "b3802ad58cdcc47b3219",
+    "url": "/react-ant-project/static/css/main.261a3ca7.chunk.css"
+  },
+  {
+    "revision": "744adbbca6078f34c2b1",
+    "url": "/react-ant-project/static/js/2.f87dd966.chunk.js"
+  },
+  {
+    "revision": "b3802ad58cdcc47b3219",
+    "url": "/react-ant-project/static/js/main.05c7a0d7.chunk.js"
+  },
+  {
+    "revision": "074e4380f1d032ea8c85",
+    "url": "/react-ant-project/static/js/runtime-main.aef9078d.js"
+  }
+]);
